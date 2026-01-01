1ae64615e2e9f8d43a0f25c2f293c4a7ac260106
tenifayo abdulsomod <tenifayoabdulsomod@gmail.com>
2026-01-01 11:14:03 -0800

    First commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
